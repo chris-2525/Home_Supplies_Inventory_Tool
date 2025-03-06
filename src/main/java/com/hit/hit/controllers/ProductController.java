2b81879572690a60318b8b39f0ad31069282aedb
Christine Ang <christineang1@gmail.com>
2025-03-06 16:19:34 -0500

    return html file

--- src/main/java/com/hit/hit/controllers/ProductController.java
+++ src/main/java/com/hit/hit/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.hit.hit.controllers;
 
 import com.hit.hit.model.Product;
 import com.hit.hit.repositories.ProductRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,19 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/product")
+//@RestController
+//@RequestMapping("/product")
+@Controller
 public class ProductController {
 
     private final ProductRepository productRepository;
 
     public ProductController(ProductRepository productRepository) {this.productRepository = productRepository;}
 
-    @GetMapping
-    public List<Product> findProducts() {return productRepository.findAllProducts();}
+//    @GetMapping
+//    public List<Product> findProducts() {return productRepository.findAllProducts();}
 
-    @PostMapping
-    public void storeProduct(@RequestBody Product product) {productRepository.storeProduct(product);}
+//    @PostMapping
+//    public void storeProduct(@RequestBody Product product) {productRepository.storeProduct(product);}
+
+    @GetMapping("/showProducts")
+    public String viewProducts(Model model) {
+        var products = productRepository.findAllProducts();
+        model.addAttribute("products", products);
+        return "product.html";
+    }
 
 
 }
